90878bd119ffa585516a77894e67f85596d5cac0
Kevin Flaherty <kf@ufst.dk>
2024-01-20 22:16:31 +0000

    added textile production and wastewater treatment processes

---
+++ textile/test/textile/core_test.clj
@@ -0,0 +1,7 @@
+(ns textile.core-test
+  (:require [clojure.test :refer :all]
+            [textile.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ wastewater/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wastewater
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
